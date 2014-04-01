b49f5bf5c718f22cf83db7a47c94c4a4b516fc21
Matt Freeman <rmf@rmfs-MacBook-Pro.local>
2014-03-31 22:40:58 -0700

    Add database cleaner support module.

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,36 @@
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner[:active_record].clean_with(:truncation)
+  end
+
+  config.before(:all) do
+    DatabaseCleaner[:active_record].strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner[:active_record].strategy = :truncation, { :pre_count => true, :reset_ids => false }
+  end
+
+  config.before(:each) do
+    DatabaseCleaner[:active_record].start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner[:active_record].clean
+  end
+
+  # uncommented because not currently working.
+  # config.after(:all, :require_cleanup) do
+  #   DatabaseCleaner[:active_record].clean_with(:truncation)
+  # end
+
+  config.after(:suite) do
+    DatabaseCleaner[:active_record].clean_with(:truncation)
+  end
+
+end
