ba055e4e0687aa9dc0340960a63449d4c75fd0ea
Matt Freeman <rmf@rmfs-MacBook-Pro.local>
2014-03-31 22:42:52 -0700

    Add commented out validations, not sure if actually required.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,8 @@ class Answer < ActiveRecord::Base
   attr_accessible :exam_id, :question_id, :status
   belongs_to :question
   belongs_to :exam
+
+  # these may be required?
+  # validate :exam_id, :presence => true
+  # validate :question_id, :presence => true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   # rails 4, must get rid of attr_accessible
   attr_accessible :body, :quiz_id, :user_id
   belongs_to :user
+
+  # these may be required?
+  # validate :quiz_id, :presence => true
+  # validate :user_id, :presence => true
 end
